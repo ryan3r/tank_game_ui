41e27b871ef2bd509866163102170a40418381f7
Ryan Ray <ryan314r@gmail.com>
2024-04-28 02:24:23 +0000

    Don't render popup off screen

--- frontend/src/ui/generic/popup.jsx
+++ frontend/src/ui/generic/popup.jsx
@@ -4,9 +4,43 @@ import { useCallback, useEffect, useState } from "preact/hooks";
 
 const POPUP_PADDING = 5;
 
+function repositionPopup(ownSize, anchorRef, setPosition) {
+    // Hide the element if we don't know it's size
+    // This also means that the opacity transition will trigger when we render it
+    if(ownSize.width === 0 && ownSize.height === 0) {
+        setPosition({
+            left: 0,
+            top: 0,
+            opacity: 0,
+        });
+        return;
+    }
+
+    const rect = anchorRef.current.getBoundingClientRect();
+    const centerOfAnchor = window.scrollY + rect.y + (rect.width / 2);
+    const leftOfAnchor = window.scrollX + rect.x;
+    const rightOfAnchor = leftOfAnchor + rect.width;
+
+    // Make sure there is enough space to the right to display the popup
+    const displayOnRightSide = (rightOfAnchor + POPUP_PADDING + ownSize.width) <= window.innerWidth;
+
+    const left = displayOnRightSide ?
+        (rightOfAnchor + POPUP_PADDING) :
+        (leftOfAnchor - ownSize.width - POPUP_PADDING);
+
+    setPosition({
+        left,
+        top: centerOfAnchor - (ownSize.height / 2),
+    });
+}
+
 export function Popup({ opened, anchorRef, children, onClose }) {
     const [position, setPosition] = useState();
     const [ownSize, setOwnSize] = useState({ width: 0, height: 0 });
+
+    const onResize = useCallback(
+        () => repositionPopup(ownSize, anchorRef, setPosition),
+        [ownSize, anchorRef, setPosition]);
 
     useEffect(() => {
         // Either we're closed or we don't have an element to attach our self to
@@ -15,29 +49,20 @@ export function Popup({ opened, anchorRef, children, onClose }) {
             return;
         }
 
-        // Hide the element if we don't know it's size
-        // This also means that the opacity transition will trigger when we render it
-        if(ownSize.width === 0 && ownSize.height === 0) {
-            setPosition({
-                left: 0,
-                top: 0,
-                opacity: 0,
-            });
-            return;
-        }
+        repositionPopup(ownSize, anchorRef, setPosition);
 
-        const rect = anchorRef.current.getBoundingClientRect();
-        const centerOfAnchor = window.scrollY + rect.y + (rect.width / 2);
-
-        setPosition({
-            left: window.scrollX + rect.x + rect.width + POPUP_PADDING,
-            top: centerOfAnchor - (ownSize.height / 2),
-        });
+        // Reposition the popup when the window gets resized
+        window.addEventListener("resize", onResize);
 
         if(onClose) {
             // Close if anything else is clicked
             window.addEventListener("click", onClose);
-            return () => window.removeEventListener("click", onClose);
+        }
+
+        return () => {
+            window.removeEventListener("resize", onResize);
+
+            if(onClose) window.removeEventListener("click", onClose);
         }
     }, [opened, anchorRef, onClose, ownSize]);
 
