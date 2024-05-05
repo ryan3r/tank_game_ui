d95df9c254d9b27f51ba92c78b54bfee642a4114
Ryan Ray <ryan314r@gmail.com>
2024-05-05 03:32:14 +0000

    Add victory message

--- common/state/game-state.mjs
+++ common/state/game-state.mjs
@@ -3,10 +3,12 @@ import Players from "./players/players.mjs";
 import { ResourceHolder } from "./resource.mjs";
 
 export class GameState {
-    constructor(players, board, councilAttributes) {
+    constructor(players, board, councilAttributes, running = true, winner = "") {
         this.players = players;
         this.board = board;
         this.council = councilAttributes;
+        this.running = running;
+        this.winner = winner?.length > 0 ? winner : undefined;
     }
 
     static deserialize(rawGameState) {
@@ -16,6 +18,8 @@ export class GameState {
             Players.deserialize(rawGameState.players, board),
             board,
             ResourceHolder.deserialize(rawGameState.council),
+            rawGameState.running,
+            rawGameState.winner,
         );
     }
 
@@ -24,6 +28,8 @@ export class GameState {
             players: this.players.serialize(),
             board: this.board.serialize(),
             council: this.council.serialize(),
+            running: this.running,
+            winner: this.winner,
         }
     }
 }
