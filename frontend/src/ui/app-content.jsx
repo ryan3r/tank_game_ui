b92a44549baa7d7fb612b7149e72e5e1937f9abe
Ryan Ray <ryan314r@gmail.com>
2024-05-09 06:17:03 +0000

    Show banner if web page is out of date

--- frontend/src/ui/app-content.jsx
+++ frontend/src/ui/app-content.jsx
@@ -1,7 +1,8 @@
-export function AppContent({ debugMode, withSidebar, toolbar, children }) {
+export function AppContent({ debugMode, withSidebar, toolbar, buildInfo, children }) {
     return (
         <div className={`app-wrapper ${withSidebar ? "with-sidebar" : ""}`}>
             {debugMode}
+            {buildInfo !== undefined ? <AutoRestart buildInfo={buildInfo}></AutoRestart> : undefined}
             {toolbar}
             <div className="app-content">
                 {children}
@@ -11,4 +12,21 @@ export function AppContent({ debugMode, withSidebar, toolbar, children }) {
             </div>
         </div>
     );
+}
+
+function AutoRestart({ buildInfo }) {
+    const isOutDated = buildInfo !== BUILD_INFO;
+
+    const reload = e => {
+        e.preventDefault();
+        location.reload();
+    };
+
+    if(isOutDated) {
+        return (
+            <div className="debug-mode-banner">
+                You're version of tank game is out of date <a href="#" onClick={reload}>reload</a> to update.
+            </div>
+        );
+    }
 }
