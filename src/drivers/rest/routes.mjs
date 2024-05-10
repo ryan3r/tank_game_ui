f07f8834e4d0f1ce559200364af7abab829af3fa
Ryan Ray <ryan314r@gmail.com>
2024-05-10 04:42:49 +0000

    Fix docker image

--- src/drivers/rest/routes.mjs
+++ src/drivers/rest/routes.mjs
@@ -3,7 +3,7 @@ import fs from "node:fs";
 import path from "node:path";
 import { logger } from "../logging.mjs";
 
-const STATIC_DIR = "../www";
+const STATIC_DIR = "www";
 
 export function defineRoutes(app, buildInfo) {
     try {
